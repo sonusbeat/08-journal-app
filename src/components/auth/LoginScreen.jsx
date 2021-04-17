c75d65c1ff03025372f4b4dea5cf02dc3776d78d
qbixmex <qbixmex@gmail.com>
2021-04-17 17:33:27 -0500

    Login Screen Styles

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,27 +1,52 @@
+import { Link } from "react-router-dom";
+
 const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className="auth__title">Login</h3>
 
       <form>
 
         <input
+          className="auth__input"
           type="text"
           placeholder="email"
           name="email"
+          autoComplete="off"
         />
 
         <input
+          className="auth__input"
           type="password"
           placeholder="password"
           name="password"
         />
 
-        <button>Login</button>
+        <button
+          className="btn btn-primary btn-block"
+        >Login</button>
 
-        <hr />
+        <div className="auth__social-networks">
 
-        <p>Google</p>
+          <h4 className="auth__subtitle">Login with social networks</h4>
+
+          <div className="google-btn">
+
+            <div className="google-icon-wrapper">
+                <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+            </div>
+
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+
+          </div>
+
+          <Link className="auth__link" to="/auth/register">
+            Create new account
+          </Link>
+
+        </div>
 
       </form>
     </>
