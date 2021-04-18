ebe49f1b78fd03d181cf091babd90ba60111c033
qbixmex <qbixmex@gmail.com>
2021-04-18 17:52:01 -0500

    Home Screen Component and Styles

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,10 +1,32 @@
 import NotesAppBar from './NotesAppBar';
+import landscape from "../../images/landscape.jpg";
 
 const NoteScreen = () => {
   return (
     <div className="notes">
-      <div className="notes__main-content">
+      <div className="notes__main">
         <NotesAppBar />
+
+        <div className="notes__content">
+
+          <input
+            className="notes__title-input"
+            placeholder="Some awesome title"
+            type="text"
+            name="title"
+            autocomplete="off"
+          />
+
+          <textarea
+            name=""
+            placeholder="What happend today ?"
+            className="notes__textarea"
+          ></textarea>
+
+          <div className="notes__image">
+            <img src={ landscape } alt="Landscape" />
+          </div>
+        </div>
       </div>
     </div>
   );
