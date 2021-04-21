71c704835480ffabf84ddcc05f9fcc8244c9a031
qbixmex <qbixmex@gmail.com>
2021-04-20 22:51:14 -0500

    Keep authentication state while reload browser

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,14 +1,27 @@
+import { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch
 } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import AuthRouter from "./AuthRouter";
 import JournalScreen from "../components/journal/JournalScreen";
+import firebase from "../firebase/firebase-config";
+import login from "../actions/auth";
 
 const AppRouter = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged( (user) => {
+      if (user?.uid) dispatch( login(user.uid, user.displayName) );
+    });
+  }, [ dispatch ]);
+
   return (
     <Router>
       <div>
