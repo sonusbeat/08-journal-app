21b22ddc483a2d0bfecdc5d867c4b0624d2981cb
qbixmex <qbixmex@gmail.com>
2021-04-21 22:09:13 -0500

    Notes Reducer

--- src/components/journal/JournalScreen.jsx
+++ src/components/journal/JournalScreen.jsx
@@ -1,15 +1,23 @@
+import { useSelector } from "react-redux";
+
 import Sidebar from "./Sidebar";
-// import NothingSelected from './NothingSelected';
+import NothingSelected from './NothingSelected';
 import NoteScreen from '../notes/NoteScreen';
 
 const JournalScreen = () => {
+
+  const { active } = useSelector(state => state.notes );
+
   return (
     <div className="journal">
       <Sidebar />
 
       <main className="journal__main-content">
-        {/* <NothingSelected /> */}
-        <NoteScreen />
+        {
+          ( active )
+          ? ( <NoteScreen /> )
+          : ( <NothingSelected /> )
+        }
       </main>
     </div>
   );
---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,15 @@
+// import types from '../types/types';
+
+const initialState = {
+  notes: [],
+  active: null
+};
+
+const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default notesReducer;
