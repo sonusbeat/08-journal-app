afff09b7476e4343644ead63c2f74dba3bb3a974
qbixmex <qbixmex@gmail.com>
2021-04-27 20:17:18 -0500

    Pruebas Asincronas en StartNewNote

---
+++ src/tests/actions/auth.test.js
@@ -0,0 +1,7 @@
+describe("Pruebas en Auth", () => {
+
+  test("Deberia *", () => {
+
+  });
+
+});
---
+++ src/tests/actions/notes.test.js
@@ -0,0 +1,21 @@
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk"
+import { startNewNote } from '../../actions/notes';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const store = mockStore({
+  auth: {
+    uid: "TESTING",
+  },
+});
+
+describe("Pruebas con las acciones de notes", () => {
+
+  test("Deberia de crear una nueva nota startNewNote", async () => {
+    await store.dispatch( startNewNote() );
+  });
+
+});
+
