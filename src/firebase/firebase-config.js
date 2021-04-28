0ff00b2de7e552a0bc38bf9ad4fb355b8e13eee3
qbixmex <qbixmex@gmail.com>
2021-04-27 22:51:37 -0500

    Environment Variables

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,31 +3,24 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhoMiCjJfJkjRQPFAfpfJTsd8xI4YfBsg",
-  authDomain: "react-app-cursos-ff956.firebaseapp.com",
-  projectId: "react-app-cursos-ff956",
-  storageBucket: "react-app-cursos-ff956.appspot.com",
-  messagingSenderId: "266056971758",
-  appId: "1:266056971758:web:2ee00eb88df5966c984aff",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyDzZaSD3JmKmhYAMJptYIfEOfb5DtVen7E",
-  authDomain: "redux-demo-88071.firebaseapp.com",
-  projectId: "redux-demo-88071",
-  storageBucket: "redux-demo-88071.appspot.com",
-  messagingSenderId: "401806799073",
-  appId: "1:401806799073:web:c8331561425c5e58c647a0"
-};
+// Referencia
+// if ( process.env.NODE_ENV === "test" ) {
+//   // Initialize Firebase "Testing"
+//   firebase.initializeApp(firebaseConfigTesting);
+// } else {
+//   // Initialize Firebase "Production"
+//   firebase.initializeApp(firebaseConfig);
+// }
 
-if ( process.env.NODE_ENV === "test" ) {
-  // Initialize Firebase "Testing"
-  firebase.initializeApp(firebaseConfigTesting);
-} else {
-  // Initialize Firebase "Production"
-  firebase.initializeApp(firebaseConfig);
-}
-
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
--- src/tests/helpers/fileUpload.test.js
+++ src/tests/helpers/fileUpload.test.js
@@ -1,30 +1,30 @@
-// import cloudinary from "cloudinary";
 import { fileUpload } from "../../helpers/fileUpload";
+import cloudinary from 'cloudinary';
 
-// cloudinary.config({
-//   cloud_name: "xxxx",
-//   api_key: "xxxx",
-//   api_secret: "xxxx",
-// });
+cloudinary.config({
+  cloud_name: process.env.REACT_APP_CLOUD_NAME,
+  api_key: process.env.REACT_APP_API_KEY,
+  api_secret: process.env.REACT_APP_API_SECRET,
+});
 
 describe("Pruebas en fileUpload", () => {
 
-  // test("Debería de cargar el archivo y retornar el URL", async () => {
+  test("Debería de cargar el archivo y retornar el URL", async () => {
 
-  //   const response = await fetch("https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png");
-  //   const blob = await response.blob();
+    const response = await fetch("https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png");
+    const blob = await response.blob();
 
-  //   const file = new File([blob], "picture.png");
+    const file = new File([blob], "picture.png");
 
-  //   const url = await fileUpload( file );
+    const url = await fileUpload( file );
 
-  //   expect( typeof url ).toBe("string");
+    expect( typeof url ).toBe("string");
 
-  //   // Borrar Imagen
-  //   const segments = url.split("/");
-  //   const imageId = segments[ segments.length - 1 ].replace(".png", "");
-  //   cloudinary.v2.api.delete_resources(imageId);
-  // });
+    // Borrar Imagen
+    const segments = url.split("/");
+    const imageId = segments[ segments.length - 1 ].replace(".png", "");
+    cloudinary.v2.api.delete_resources(imageId);
+  });
 
   test("Debería de retornar un error", async () => {
 
