4ffdd00521448ac0213a274d1a19f854cf743f41
qbixmex <qbixmex@gmail.com>
2021-04-29 21:09:39 -0500

    Pruebas en Login y Google Login

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import useForm from '../../hooks/useForm';
-import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
-import { setError, unsetError } from '../../actions/ui';
+import useForm from "../../hooks/useForm";
+import { startGoogleLogin, startLoginEmailPassword } from "../../actions/auth";
+import { setError, unsetError } from "../../actions/ui";
 import validator from "validator";
 
 const LoginScreen = () => {
   const dispatch = useDispatch();
-  const { msgError, loading } = useSelector( state => state.ui );
+  const { msgError, loading } = useSelector(state => state.ui);
 
-  const [ formValues, handleInputChange ] = useForm({
+  const [formValues, handleInputChange] = useForm({
     email: "",
     password: "",
   });
@@ -17,109 +17,100 @@ const LoginScreen = () => {
   const { email, password } = formValues;
 
   const isFormValid = () => {
-    if ( validator.isEmpty( email ) ) {
-      dispatch( setError("Email should be present !") );
+    if (validator.isEmpty(email)) {
+      dispatch(setError("Email should be present !"));
       return false;
-    } else  if ( !validator.isEmail( email ) ) {
-      dispatch( setError("Email format not valid !") );
+    } else if (!validator.isEmail(email)) {
+      dispatch(setError("Email format not valid !"));
       return false;
-    } else  if ( validator.isEmpty( password ) ) {
-      dispatch( setError("Password should be present !") );
+    } else if (validator.isEmpty(password)) {
+      dispatch(setError("Password should be present !"));
       return false;
-    } else  if ( password.trim().length < 8 ) {
-      dispatch( setError("Password should be greater than 8 characters !") );
+    } else if (password.trim().length < 8) {
+      dispatch(setError("Password should be greater than 8 characters !"));
       return false;
     }
 
-    dispatch( unsetError() );
+    dispatch(unsetError());
 
     return true;
   };
 
-  const handleLogin = (e) => {
+  const handleLogin = e => {
     e.preventDefault();
 
-    if( isFormValid() ) {
-      dispatch( startLoginEmailPassword( email, password ) );
+    if (isFormValid()) {
+      dispatch(startLoginEmailPassword(email, password));
     }
   };
 
   const handleGoogleLogin = () => {
-    dispatch( startGoogleLogin() );
+    dispatch(startGoogleLogin());
   };
-
 
   return (
     <>
-      {
-        msgError &&
-        (
-          <div className="auth__alert-error mb-2 text-center">
-            { msgError }
-          </div>
-        )
-      }
+      {msgError && (
+        <div className="auth__alert-error mb-2 text-center">{msgError}</div>
+      )}
 
       <h3 className="auth__title">Login</h3>
 
       <form
-        onSubmit={ handleLogin }
+        onSubmit={handleLogin}
         className="animate__animated animate__fadeIn animate__faster"
       >
-
         <input
           name="email"
-          value={ email }
+          value={email}
           className="auth__input"
           type="text"
           placeholder="email"
           autoComplete="off"
           autofill="off"
-          onChange={ handleInputChange }
+          onChange={handleInputChange}
         />
 
         <input
           name="password"
-          value={ password }
+          value={password}
           className="auth__input"
           type="password"
           placeholder="password"
           autoComplete="off"
           autofill="off"
-          onChange={ handleInputChange }
+          onChange={handleInputChange}
         />
 
         <button
           type="submit"
           className="btn btn-primary btn-block"
-          disabled={ loading }
-        >Login</button>
+          disabled={loading}
+        >
+          Login
+        </button>
 
         <div className="auth__social-networks">
-
           <h4 className="auth__subtitle">Login with social networks</h4>
 
-          <div
-            className="google-btn"
-            onClick={ handleGoogleLogin }
-          >
-
+          <div className="google-btn" onClick={handleGoogleLogin}>
             <div className="google-icon-wrapper">
-                <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
             </div>
 
             <p className="btn-text">
               <b>Sign in with google</b>
             </p>
-
           </div>
 
           <Link className="auth__link link" to="/auth/register">
             Create new account
           </Link>
-
         </div>
-
       </form>
     </>
   );
--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -6,10 +6,22 @@ import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
 
 import LoginScreen from "../../../components/auth/LoginScreen";
+import { startGoogleLogin, startLoginEmailPassword } from "../../../actions/auth";
+
+jest.mock("../../../actions/auth", function() {
+  return {
+    // Simular llamar la acción de startGoogleLogin con jest.fn()
+    startGoogleLogin: jest.fn(),
+
+    // Simular llamar la acción de startLoginEmailPassword con jest.fn()
+    startLoginEmailPassword: jest.fn(),
+  };
+});
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
+// Estado inicial del State de Redux
 const initialState = {
   auth: {},
   ui: {
@@ -19,6 +31,9 @@ const initialState = {
 };
 
 let store = mockStore( initialState );
+
+// Simular el dispatch para las pruebas
+store.dispatch = jest.fn();
 
 const wrapper = mount(
   <Provider store={ store }>
@@ -31,13 +46,58 @@ const wrapper = mount(
 describe("Pruebas en <LoginScreen />", () => {
 
   beforeEach(() => {
+    // Limpiar el store volviendolo a sus valores iniciales
     store = mockStore( initialState );
+
+    // Buena practica limpiar todos los Mocks
+    jest.clearAllMocks();
   });
 
   test("Deberia de coincidir con el Snapshot", () => {
 
     expect( wrapper ).toMatchSnapshot();
 
+  });
+
+  test('Debe de disparar la acción de startGoogleLogin', () => {
+    // 1. Buscar el botón de Google y simular el evento click
+    wrapper.find(".google-btn").prop("onClick")();
+
+    // 2. Probar que la acción halle sido llamado por lo menos 1 vez
+    expect( startGoogleLogin ).toHaveBeenCalled();
+  });
+
+  test("Deberia de disparar el startLogin con los respectivos argumentos", () => {
+    // 1. Inicializar el correo electronico
+    const email = "test@testing.com";
+
+    // 2. Inicializar la contraseña
+    const password = "0123456789";
+
+    // 3. Simular el llenado del campo de email
+    wrapper.find('input[name="email"]').simulate('change', {
+      target: {
+        name: "email", // Nota asi debe de llamarse en tu formulario
+        value: email, // El valor lo tomo de la variable
+      }
+    });
+
+    // 4. Simular el llenado del campo del password
+    wrapper.find('input[name="password"]').simulate('change', {
+      target: {
+        name: "password", // Nota asi debe de llamarse en tu formulario
+        value: password, // El valor lo tomo de la variable
+      }
+    });
+
+    // 4. Buscar el botón de Google y simular el evento click
+    wrapper.find("form").prop("onSubmit")({
+      preventDefault(){}
+    });
+
+
+    // 5. Probar que la acción halle sido llamada
+    expect( startLoginEmailPassword ).toHaveBeenCalledWith(email, password);
   });
 
 });
